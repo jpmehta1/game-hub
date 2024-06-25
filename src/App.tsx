f9d8e9bc82797b1162d03903540f681f47a17201
jpmehta1 <mehtajeetparesh@gmail.com>
2024-06-25 22:33:16 +0530

    Build color toggle switch

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,9 @@ return <Grid templateAreas={{
 <NavigationBar></NavigationBar>
 </GridItem>
 <Show above = "lg">
-<GridItem  area = 'aside' bg = 'gold'>Aside</GridItem>
+<GridItem  area = 'aside'>Aside</GridItem>
 </Show>
-<GridItem  area = 'main' bg = 'dodgerblue'>Main</GridItem>
+<GridItem  area = 'main'>Main</GridItem>
 </Grid>
 
 }
