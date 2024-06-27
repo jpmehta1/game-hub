e4e33b636ee43b90f3ff9fe3985c35f70997ebbf
jpmehta1 <mehtajeetparesh@gmail.com>
2024-06-27 22:34:10 +0530

    Sort the games

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import SortSelector from './components/SortSelector';
 export interface GameQuery{
   genre: Genre| null
   platform : Platform|null
+  sortOrder: string
 }
 
 
@@ -42,7 +43,7 @@ paddingX = {5}
 <GridItem  area = 'main'>
   <HStack spacing = {5} paddingLeft = {2} marginBottom = {5}>
     <PlatformSelector selectedPlatform = {gameQuery.platform} onSelectPlatform={(platform)=>setGameQuery({...gameQuery, platform})}></PlatformSelector>
-    <SortSelector></SortSelector>
+    <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery,sortOrder})}></SortSelector>
   </HStack>
   <GameGrid gameQuery={gameQuery}></GameGrid>
 </GridItem>
--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,34 +1,40 @@
-import React from 'react'
-import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
-import { BsChevronDown } from 'react-icons/bs'
-import { Platform } from '../hooks/useGame'
-import usePlatforms from '../hooks/usePlatforms'
+import React from "react";
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+import { Platform } from "../hooks/useGame";
+import usePlatforms from "../hooks/usePlatforms";
 
-
-interface Props{
-    onSelectPlatform: (platform: Platform) => void
-    selectedPlatform: Platform | null
+interface Props {
+  onSelectSortOrder:(sortOrder: string)=>void
+  sortOrder:string
 }
-const SortSelector = () => {
-  const { data,error } = usePlatforms()
-  if(error) return null
+const SortSelector = ({onSelectSortOrder,sortOrder}: Props) => {
+  const sortOrders = [
+    { value: "", label: "Relevance" },
+    { value: "-added", label: "Date Added" },
+    { value: "Name", label: "Name" },
+    { value: "-released", label: "Released Date" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Average Rating" },
+  ];
+const currentSortOrder = sortOrders.find(order => order.value === sortOrder)
+
+  const { data, error } = usePlatforms();
+  if (error) return null;
   return (
     <Menu>
-        <MenuButton as = {Button} rightIcon = {<BsChevronDown></BsChevronDown>}> 
-      Order by: Relevance
-        </MenuButton>
-        <MenuList>
-        <MenuItem> Relevance </MenuItem>
-        <MenuItem> Date Added </MenuItem>
-        <MenuItem> Name </MenuItem>
-        <MenuItem> Release Date </MenuItem>
-        <MenuItem> Popularity </MenuItem>
-        <MenuItem> Averaghj- pe Rating </MenuItem>
-        </MenuList>
-        
+      <MenuButton as={Button} rightIcon={<BsChevronDown></BsChevronDown>}>
+        Order by: {currentSortOrder?.label || 'Relevance'}
+      </MenuButton>
+      <MenuList>
+        {sortOrders.map((order) => (
+          <MenuItem onClick = {()=> onSelectSortOrder(order.value)}key={order.value} value={order.value}>
+            {order.label}
+          </MenuItem>
+        ))}
+      </MenuList>
     </Menu>
-  )
-}
+  );
+};
 
-
-export default SortSelector
+export default SortSelector;
