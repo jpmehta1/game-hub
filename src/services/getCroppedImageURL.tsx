90e1066c345a9fe53e481c2e21cb3e7f2dab3459
jpmehta1 <mehtajeetparesh@gmail.com>
2024-06-27 22:37:59 +0530

    no image placeholder added

--- src/services/getCroppedImageURL.tsx
+++ src/services/getCroppedImageURL.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import noImage from '../assets/no-image-placeholder.webp'
 
 const getCroppedImageURL = (url:string) => {
-if(!url) return '';
+if(!url) return noImage;
 
 const target = 'media/'
 const index = url.indexOf(target) + target.length
